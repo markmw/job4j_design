de575de0deca09df1e409510c4c016a72f6ae155
markmw <dant1kkz@gmail.com>
2022-03-07 16:32:38 +0600

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+        assertNull(config.value("surname"));
+    }
+
+    @Test
+    public void whenPair() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"),
+                is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("hibernate.connection.url"),
+                is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenIllegalArgumentException() {
+        String path = "./data/pair_with_exception.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
