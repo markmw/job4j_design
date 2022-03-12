d0727de5230209adf6739935dcf064d3d567eee1
markmw <dant1kkz@gmail.com>
2022-03-12 17:04:03 +0600

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Stack;
+
+public class Analizy {
+    public static void unavailable(String source, String target) {
+        try (
+                BufferedReader in = new BufferedReader(new FileReader(source));
+                PrintWriter out = new PrintWriter(new FileOutputStream(target))
+        ) {
+            Stack<String> temp = new Stack<>();
+            in.lines()
+                    .map(line -> line.split(" "))
+                    .forEach(line -> {
+                        if (("400".equals(line[0]) || "500".equals(line[0])) && temp.isEmpty()) {
+                            temp.push(line[1]);
+                        } else if ("200".equals(line[0]) && temp.size() == 1) {
+                            String rsl = temp.pop() + ";" + line[1];
+                            out.println(rsl);
+                        }
+                    });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        unavailable("./data/server.log", "./data/unavalaible.csv");
+    }
+}
