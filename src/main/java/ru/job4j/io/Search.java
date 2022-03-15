42c6443ad087d0b5f1b8d85b949fbfe466f90b55
markmw <dant1kkz@gmail.com>
2022-03-15 18:20:45 +0600

    5. Валидация параметров запуска. [#246865 #274721]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,7 +11,9 @@ import java.util.List;
 public class Search {
     public static void main(String[] args) throws IOException {
         if (args.length == 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar SearchProgram.jar ROOT_FOLDER.");
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar SearchProgram.jar ROOT_FOLDER .FILE_EXTENSION"
+            );
         }
         Path start = Paths.get(args[0]);
         File file = start.toFile();
